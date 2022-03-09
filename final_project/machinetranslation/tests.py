908efc577307c5a718c9eaf9c6b022ad303f805e
Lev Tv <leff.tv@gmail.com>
2022-03-09 09:43:48 +0000

    -

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,8 @@
+import translator
+
+#print(translator.french_to_english(None))
+#print(translator.english_to_french(None))
+print(translator.french_to_english('Hello'))
+print(translator.french_to_english('Bonjour'))
+print(translator.english_to_french('Hello'))
+print(translator.english_to_french('Bonjour'))
